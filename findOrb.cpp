a2795423e878c50910d84d4bc5586058f4bc21c5
Katherine Yang <katherine_yang@hmc.edu>
2017-04-20 13:37:41 -0500

    finished code but need linux computer

--- findOrb.cpp
+++ findOrb.cpp
@@ -18,43 +18,80 @@ int main(int argc, char** argv) {
   if ( (!im1.data || !im2.data) ){
     printf("No image data \n");
     return -1;
-   }
-   Mat des1, des2;
-   vector<cv::KeyPoint> keyp1, keyp2;
-   unsigned int descriptor_size = 0, distance_threshold = 0, score_threshold = 0;
-      
-    if (USE_ORB) {
-      descriptor_size = 32;
-      distance_threshold = 50;
-      score_threshold = 205;
-      EyeMARS::orbextractor orb_extractor;
+  }
+  Mat des1, des2;
+  vector<cv::KeyPoint> keyp1, keyp2;
+  unsigned int descriptor_size = 0, distance_threshold = 0, score_threshold = 0;
+  
+  if (USE_ORB) {
+    descriptor_size = 32;
+    distance_threshold = 50;
+    score_threshold = 205;
+    EyeMARS::orbextractor orb_extractor;
 
-      orb_extractor.ExtractKeypointsDescriptors(im1, keyp1, des1);
-      orb_extractor.ExtractKeypointsDescriptors(im2, keyp2, des2);
-    } else {
+    orb_extractor.ExtractKeypointsDescriptors(im1, keyp1, des1);
+    orb_extractor.ExtractKeypointsDescriptors(im2, keyp2, des2);
+  } else {
       // freak
-     descriptor_size = 64;
-     distance_threshold = 100;
-     score_threshold = 480;
-     EyeMARS::FREAKExtractor freak_extractor;
-     
-     freak_extractor.FindAndExtractFreakFeatures(im1, keyp1, des1);
-     freak_extractor.FindAndExtractFreakFeatures(im2, keyp2, des2);
-    }
-  
-
-
+   descriptor_size = 64;
+   distance_threshold = 100;
+   score_threshold = 480;
+   EyeMARS::FREAKExtractor freak_extractor;
    
-  namedWindow("Image 1", WINDOW_AUTOSIZE );
-  namedWindow("Image 2", WINDOW_AUTOSIZE );
-  Mat imDraw1, imDraw2;
-  drawKeypoints(im1, keyp1, imDraw1);
-  drawKeypoints(im2, keyp2, imDraw2);
-  imshow("Image 1", imDraw1);
-  imshow("Image 2", imDraw2);
-  
-  cout << "numFeature1 "<< keyp1.size();
-  cout << " numFeature2 "<< keyp2.size()<<endl;
+   freak_extractor.FindAndExtractFreakFeatures(im1, keyp1, des1);
+   freak_extractor.FindAndExtractFreakFeatures(im2, keyp2, des2);
+ }
+ 
+ namedWindow("Image 1", WINDOW_AUTOSIZE );
+ namedWindow("Image 2", WINDOW_AUTOSIZE );
+ Mat imDraw1, imDraw2;
+ drawKeypoints(im1, keyp1, imDraw1);
+ drawKeypoints(im2, keyp2, imDraw2);
+ imshow("Image 1", imDraw1);
+ imshow("Image 2", imDraw2);
+ 
+ cout << "numFeature1 "<< keyp1.size();
+ cout << " numFeature2 "<< keyp2.size()<<endl;
+
+
+  // Find matches
+
+ cv::BFMatcher matcher(cv::NORM_HAMMING);
+ std::vector<cv::DMatch> matches;
+ if(des1.type()!=CV_32F) des1.convertTo(des1, CV_32F);
+ if(des2.type()!=CV_32F) des2.convertTo(des2, CV_32F);
+ matcher.match(des1, des2 matches);
+ double max_dist = 0; double min_dist = 100;
+ 
+ cout << "found "<<matches.size()<< " matches"<<endl;
+   //-- Quick calculation of max and min distances between keypoints
+ for( int i = 0; i < des1.rows; i++ ){
+  double dist = matches[i].distance;
+  if( dist < min_dist ) min_dist = dist;
+  if( dist > max_dist ) max_dist = dist;
+}
+printf("-- Max dist : %f \n", max_dist );
+printf("-- Min dist : %f \n", min_dist );
+
+
+  //-- Draw only "good" matches (i.e. whose distance is less than 2*min_dist,
+  //-- or a small arbitary value ( 0.02 ) in the event that min_dist is very
+  //-- small)
+  //-- PS.- radiusMatch can also be used here.
+std::vector< DMatch > good_matches;
+for( int i = 0; i < des1.rows; i++ ) { 
+//    if( matches[i].distance <= max(2*min_dist, 0.02) ){
+  if( matches[i].distance <= 150){
+    good_matches.push_back( matches[i]); }
+  }
+  //-- Draw only "good" matches
+  cout << "drawing good matches...";
+  cout << good_matches.size()<< " good matches"<<endl; 
+  Mat img_matches;
+  drawMatches( im1, keyp1, im2, keyp2,
+   good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+   vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+
   /* 
   // greedy matches
    FlannBasedMatcher matcher;
