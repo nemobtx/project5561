a216c9bd53315dca8ced17dc22b7be714a7b7028
Tong Ke <tonyke1993@gmail.com>
2017-04-26 16:11:57 -0500

    equalization

---
+++ eqTest.cpp
@@ -0,0 +1,26 @@
+//
+// Created by tong on 4/26/17.
+//
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+
+int main() {
+  Mat img1 = imread("../imgs/exposure0/1.jpg", cv::IMREAD_GRAYSCALE);
+  Mat img2 = imread("../imgs/exposure-1/1.jpg", cv::IMREAD_GRAYSCALE);
+
+  Mat img1_eq, img2_eq;
+  equalizeHist(img1, img1_eq);
+  equalizeHist(img2, img2_eq);
+
+  imwrite("../imgs/exposure0/1_eq.jpg", img1_eq);
+  imwrite("../imgs/exposure-1/1_eq.jpg", img2_eq);
+
+  imshow("1", img1_eq);
+  imshow("2", img2_eq);
+  waitKey();
+}
